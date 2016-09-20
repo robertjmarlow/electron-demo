bf1713892d1eede7bc15467e66e9ae5282559195
rm025953 <robert.marlow@cerner.com>
2016-09-20 10:11:54 -0500

    Adding a build profile

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ app.on('window-all-closed', function() {
 app.on('ready', function() {
   mainWindow = new BrowserWindow({ width: 1360, height: 800 });
   mainWindow.loadURL('file://' + __dirname + '/public/index.html');
-  mainWindow.openDevTools();
+  // mainWindow.openDevTools();
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
